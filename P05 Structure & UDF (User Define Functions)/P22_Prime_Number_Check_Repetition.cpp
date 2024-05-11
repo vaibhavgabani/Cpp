8b690eaf93f6470f749dcc2107cc0948a0b2ae1c
Vaibhav Gabani <97325474+vaibhavgabani@users.noreply.github.com>
2024-05-11 11:34:31 +0530

    Add files via upload

---
+++ P05 Structure & UDF (User Define Functions)/P22_Prime_Number_Check_Repetition.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int prime(int);
+
+int main() {
+    int n;
+    cout << "Enter Number: ";
+    cin >> n;
+    int total = prime(n);
+    if (total == 1) {
+        cout << "Entered number is prime." << endl;
+    } else {
+        cout << "Entered number is not prime." << endl;
+    }
+    return 0;
+}
+
+int prime(int n) {
+    int cnt = 0;
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            cnt++;
+        }
+    }
+    if (cnt == 2) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
