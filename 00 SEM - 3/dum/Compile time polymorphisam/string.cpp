2f4f7398eb7acd0eccd6d3228e52294dce980d8a
Vaibhav Gabani <97325474+vaibhavgabani@users.noreply.github.com>
2024-10-20 17:56:17 +0530

    Update string.cpp

--- 00 SEM - 3/dum/Compile time polymorphisam/string.cpp
+++ 00 SEM - 3/dum/Compile time polymorphisam/string.cpp
@@ -1,84 +1,80 @@
 #include <iostream>
-#include<cstring>
+#include <cstring>
 using namespace std;
 
-class String{
-    public:
+class String {
+public:
     char str[20];
     int size = 0;
 
-    void get(){
-        cout<<"Enter Here : ";
-        cin>>str;
-        size = strlen(str);
-    }
-    void display(){
-        cout<<str<<endl;
+    String() {
+        str[0] = '\0'; // Initialize to empty string
     }
 
-    String operator +(String a){
+    // Overload input stream operator
+    friend istream& operator>>(istream& in, String& a) {
+        cout << "Enter Here: ";
+        in >> a.str;
+        a.size = strlen(a.str);
+        return in;
+    }
+
+    // Overload output stream operator
+    friend ostream& operator<<(ostream& out, const String& a) {
+        out << a.str;
+        return out;
+    }
+
+    // Other member functions remain the same
+
+    String operator+(String a) {
         String tmp;
-        tmp.size = size + a.size;
-        strcat(tmp.str,str);
-        strcpy(tmp.str,a.str);
+        strcpy(tmp.str, str); // Copy current string
+        strcat(tmp.str, a.str); // Concatenate the other string
+        tmp.size = strlen(tmp.str); // Update size
         return tmp;
     }
 
-    int operator ==(String a){
-        if(strcmp(str,a.str) == 0){
-            return 0;
-        }
-        return 1;
+    int operator==(String a) {
+        return strcmp(str, a.str) == 0; // Returns 1 if equal, 0 if not
     }
 
-    int operator !=(String a){
-        if(strcmp(str,a.str) == 0){
-            return 0;
-        }
-        return 1;
-    }
-    
-    int operator <=(String a){
-        if(size <= a.size){
-            return 0;
-        }
-        return 1;
-    }
-    
-    int operator <(String a){
-        if(size < a.size){
-            return 1;
-        }
-        return 0;
+    int operator!=(String a) {
+        return strcmp(str, a.str) != 0; // Returns 1 if not equal, 0 if equal
     }
 
-    int operator >=(String a){
-        if(size >= a.size){
-            return 0;
-        }
-        return 1;
+    int operator<=(String a) {
+        return size <= a.size; // Returns 1 if less than or equal
     }
 
-    int operator >(String a){
-        if(size > a.size){
-            return 0;
-        }
-        return 1;   
+    int operator<(String a) {
+        return size < a.size; // Returns 1 if less
+    }
+
+    int operator>=(String a) {
+        return size >= a.size; // Returns 1 if greater than or equal
+    }
+
+    int operator>(String a) {
+        return size > a.size; // Returns 1 if greater
     }
 };
 
 int main() {
-   String s1,s2,s3;
-   s1.get();
-   s1.display();
-   s2.get();
-   s2.display();
-    
+    String s1, s2, s3;
+
+    cin >> s1; // Using overloaded >> operator
+    cout << s1 << endl; // Using overloaded << operator
+
+    cin >> s2; // Using overloaded >> operator
+    cout << s2 << endl; // Using overloaded << operator
+
     cout << "s1 == s2: " << (s1 == s2) << endl;
     cout << "s1 != s2: " << (s1 != s2) << endl;
     cout << "s1 < s2: " << (s1 < s2) << endl;
     cout << "s1 <= s2: " << (s1 <= s2) << endl;
     cout << "s1 > s2: " << (s1 > s2) << endl;
     cout << "s1 >= s2: " << (s1 >= s2) << endl;
+
     return 0;
 }
